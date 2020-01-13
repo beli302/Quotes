bc9d5dd5a558b7525b5e967da893a71ce9a95dc0
beli302 <belindashirkiz2@gmail.com>
2020-01-13 14:18:49 +0300

    added pipe date-count

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1, 'There is no charm equal to tenderness of heart.', ''),
-    new Quote(2,'Learning never exhausts the mind.','The control of mind'),
-    new Quote(3,'Let us sacrifice our today so that our children can have a better tomorrow.', 'Our todays actions determines our future'),
-    new Quote(4,'There is nothing permanent except change.', 'Change can only be made when one accepts to change'),
-    new Quote(5,'But man is not made for defeat. A man can be destroyed but not defeated.', 'Once destroy one can sill rise up'),
+    new Quote(1, 'There is no charm equal to tenderness of heart.', 'Jane Austen',new Date(2020,1,2)),
+    new Quote(2,'Learning never exhausts the mind.','The control of mind',new Date(2020,1,4)),
+    new Quote(3,'Let us sacrifice our today so that our children can have a better tomorrow.', 'Our todays actions determines our future',new Date(2020,1,6)),
+    new Quote(4,'There is nothing permanent except change.', 'Change can only be made when one accepts to change',new Date(2020,1,8)),
+    new Quote(5,'But man is not made for defeat. A man can be destroyed but not defeated.', 'Once destroy one can sill rise up',new Date(2020,1,11)),
   ];
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
